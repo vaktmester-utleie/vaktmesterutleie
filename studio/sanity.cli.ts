71f19edd0be40b5f7c1e4cec4fa0443f10c72037
Jonathan Braaten Mjøs <jonathan@mjos.no>
2026-05-09 08:12:35 +0200

    fix: add app id

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,15 +1,16 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'w16nugsv',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
+    appId: 'abvqdb86mgkfxd0dudyqa2qb',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
-  }
+  },
 })
