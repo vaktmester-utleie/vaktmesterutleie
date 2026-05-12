b589de398bfccefa6750f62528f009304faae1fc
Jonathan Braaten Mjøs <jonathan@mjos.no>
2026-05-12 13:53:30 +0200

    feat: about us page

---
+++ web/src/app/om-oss/page.tsx
@@ -0,0 +1,28 @@
+import Container from '@/components/wrapper/container'
+import Section from '@/components/wrapper/section'
+import Image from 'next/image'
+
+export default function Page() {
+  return (
+    <Section className=" page-section  min-h-[80vh] flex items-center justify-center">
+      <Section className="grid grid-cols-1 sm:grid-cols-2 gap-4" constraint>
+        <Image
+          className="rounded-lg shadow-sm"
+          src={'/sivert.jpg'}
+          width={500}
+          height={500}
+          alt="bilde av sivert sørås"
+        />
+        <Container>
+          <h1 className="title uppercase">Om oss</h1>
+          <p className="subtitle">
+            Sørås Vaktmesterutleie vart etablert i 2019. Med fagbrev som tømrer
+            og driftstid som vaktmester har ein opparbeida seg ein brei
+            kompetanse i håndtverkarbransjen. Ved behov har me faste innleide
+            sammarbeidspartnara.
+          </p>
+        </Container>
+      </Section>
+    </Section>
+  )
+}
--- web/src/components/navigation/navigation-items.ts
+++ web/src/components/navigation/navigation-items.ts
@@ -2,6 +2,6 @@ export const navigationItems = [
   { id: 1, label: 'Heim', href: '/' },
   { id: 2, label: 'Utleige', href: '/utleige' },
   { id: 3, label: 'Prosjekt', href: '/prosjekt' },
-  { id: 4, label: 'Om oss', href: 'om-oss' },
+  { id: 4, label: 'Om oss', href: '/om-oss' },
   { id: 5, label: 'Kontakt', href: '/kontakt' },
 ]
