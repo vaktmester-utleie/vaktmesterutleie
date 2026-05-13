149023d03596454227a5ebd8d6125587ee4b9ca5
Jonathan Braaten Mjøs <jonathan@mjos.no>
2026-05-13 14:01:17 +0200

    fix: responsive cta

--- web/src/components/cta/index.tsx
+++ web/src/components/cta/index.tsx
@@ -10,22 +10,22 @@ type Props = {
 }
 export default function CTA({ subtitle, title, description }: Props) {
   return (
-    <Container className="bg-gray-100 py-40">
+    <Container className="bg-gray-100 py-40 ">
       <Container className="max-w-xl mx-auto  text-center flex flex-col gap-4">
         <p className="uppercase link-brand">{subtitle}</p>
         <p className="hero-title">{title}</p>
         <p>{description}</p>
 
-        <Container className="flex gap-4  items-center justify-center">
-          <Button asChild>
-            <Link href={'/kontakt'}>
-              <span> Send førespurnad</span>
+        <Container className="flex flex-col sm:flex-row gap-4 items-center justify-center w-full px-4">
+          <Button asChild className="w-full sm:w-auto">
+            <Link href="/kontakt">
+              <span>Send førespurnad</span>
               <Send />
             </Link>
           </Button>
-          <Button variant="outline" asChild>
+          <Button variant="outline" asChild className="w-full sm:w-auto">
             <Link href="tel:+4747177466">
-              <span> Ring oss direkte</span>
+              <span>Ring oss direkte</span>
               <Phone />
             </Link>
           </Button>
