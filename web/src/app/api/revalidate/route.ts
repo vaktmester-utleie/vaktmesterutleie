59e55af36710371e1bbb5c9075161e30daac5184
Jonathan Braaten Mjøs <jonathan@mjos.no>
2026-06-12 12:42:19 +0200

    fix

---
+++ web/src/app/api/revalidate/route.ts
@@ -0,0 +1,15 @@
+// app/api/revalidate/route.ts
+import { revalidatePath } from 'next/cache'
+import { type NextRequest, NextResponse } from 'next/server'
+
+export async function POST(req: NextRequest) {
+  const secret = req.nextUrl.searchParams.get('secret')
+
+  if (secret !== process.env.SANITY_REVALIDATE_SECRET) {
+    return NextResponse.json({ message: 'Invalid secret' }, { status: 401 })
+  }
+
+  revalidatePath('/', 'layout')
+
+  return NextResponse.json({ revalidated: true })
+}
